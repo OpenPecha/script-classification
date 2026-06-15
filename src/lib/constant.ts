59e03baa23e88bc0b666637572c6959ea45073b2
Jampaa <106014359+Jampaa@users.noreply.github.com>
2026-06-15 16:48:49 +0530

    logout

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -2,4 +2,5 @@ export const APPLICATION_NAME = 'scriptclassification'
 
 export const APPLICATION_URLS: Record<string, string> = {
   imagetranscription: 'https://image-transcription.onrender.com',
+  imagetranscriptionv2: 'https://image-transcription-v2.onrender.com',
 }
